Freescale/meta-freescale-3rdparty

commit 80be1601842ac9d911ef1ce52591acd770c80725

u-boot-script-toradex: Set virtual/bootscript as provider
Add virtual/bootscript to PROVIDES variable to make easier to use other
boot script just changing PREFERRED_PROVIDER_virtual/bootscript variable.

Signed-off-by: Fabio Berton <fabio.berton@ossystems.com.br>

conf/machine/apalis-imx6.conf
@@ -21,6 +21,7 @@ PREFERRED_PROVIDER_u-boot ?= "u-boot-toradex"
 PREFERRED_PROVIDER_virtual/bootloader ?= "u-boot-toradex"
 PREFERRED_PROVIDER_u-boot-fw-utils ?= "u-boot-toradex-fw-utils"
 PREFERRED_RPROVIDER_u-boot-fw-utils ?= "u-boot-toradex-fw-utils"
+PREFERRED_PROVIDER_virtual/bootscript ?= "u-boot-script-toradex"
 
 SPL_BINARY = "SPL"
 # The SPL configures the DDR RAM depending on the module it runs on. Thus there
@@ -33,7 +34,7 @@ UBOOT_ENTRYPOINT_use-mainline-bsp = "0x10008000"
 IMAGE_FSTYPES += "tar.xz"
 # wic support
 IMAGE_BOOT_FILES_append = " boot.scr-${MACHINE};boot.scr"
-WKS_FILE_DEPENDS_append = " u-boot-script-toradex"
+WKS_FILE_DEPENDS_append = " virtual/bootscript"
 WKS_FILE = "sdimage-bootpart.wks"
 
 MACHINE_FEATURES += "screen usbgadget usbhost vfat ext2 alsa touchscreen wifi bluetooth 3g pci"

conf/machine/colibri-imx6.conf
@@ -22,6 +22,7 @@ PREFERRED_PROVIDER_u-boot ?= "u-boot-toradex"
 PREFERRED_PROVIDER_virtual/bootloader ?= "u-boot-toradex"
 PREFERRED_PROVIDER_u-boot-fw-utils ?= "u-boot-toradex-fw-utils"
 PREFERRED_RPROVIDER_u-boot-fw-utils ?= "u-boot-toradex-fw-utils"
+PREFERRED_PROVIDER_virtual/bootscript ?= "u-boot-script-toradex"
 
 SPL_BINARY = "SPL"
 UBOOT_CONFIG ??= "spl"
@@ -32,7 +33,7 @@ UBOOT_ENTRYPOINT_use-mainline-bsp = "0x10008000"
 IMAGE_FSTYPES += "tar.xz"
 # wic support
 IMAGE_BOOT_FILES_append = " boot.scr-${MACHINE};boot.scr"
-WKS_FILE_DEPENDS_append = " u-boot-script-toradex"
+WKS_FILE_DEPENDS_append = " virtual/bootscript"
 WKS_FILE = "sdimage-bootpart.wks"
 
 MACHINE_FEATURES += "screen usbgadget usbhost vfat ext2 alsa touchscreen wifi bluetooth 3g"

conf/machine/colibri-imx6ull.conf
@@ -19,6 +19,7 @@ PREFERRED_PROVIDER_u-boot ?= "u-boot-toradex"
 PREFERRED_PROVIDER_virtual/bootloader ?= "u-boot-toradex"
 PREFERRED_PROVIDER_u-boot-fw-utils ?= "u-boot-toradex-fw-utils"
 PREFERRED_RPROVIDER_u-boot-fw-utils ?= "u-boot-toradex-fw-utils"
+PREFERRED_PROVIDER_virtual/bootscript ?= "u-boot-script-toradex"
 
 UBOOT_BINARY = "u-boot-nand.imx"
 UBOOT_MAKE_TARGET = "u-boot.imx"
@@ -27,7 +28,7 @@ UBOOT_MACHINE ?= "colibri-imx6ull_defconfig"
 IMAGE_FSTYPES += "tar.xz"
 # wic support
 IMAGE_BOOT_FILES_append = " boot.scr-${MACHINE};boot.scr"
-WKS_FILE_DEPENDS_append = " u-boot-script-toradex"
+WKS_FILE_DEPENDS_append = " virtual/bootscript"
 WKS_FILE = "sdimage-bootpart.wks"
 
 MACHINE_FEATURES += "screen usbgadget usbhost vfat ext2 alsa touchscreen wifi bluetooth 3g"

conf/machine/colibri-imx7-emmc.conf
@@ -18,10 +18,11 @@ RDEPENDS_${KERNEL_PACKAGE_NAME}-base = ""
 PREFERRED_PROVIDER_u-boot ?= "u-boot-toradex"
 PREFERRED_PROVIDER_virtual/bootloader ?= "u-boot-toradex"
 PREFERRED_PROVIDER_u-boot-fw-utils ?= "u-boot-toradex-fw-utils"
+PREFERRED_PROVIDER_virtual/bootscript ?= "u-boot-script-toradex"
 UBOOT_MACHINE ?= "colibri_imx7_emmc_defconfig"
 
 IMAGE_FSTYPES += "tar.xz"
-WKS_FILE_DEPENDS_append = " u-boot-script-toradex"
 IMAGE_BOOT_FILES_append = " boot.scr-${MACHINE};boot.scr"
+WKS_FILE_DEPENDS_append = " virtual/bootscript"
 
 MACHINE_FEATURES += "screen usbgadget usbhost vfat ext2 alsa touchscreen wifi bluetooth 3g"

conf/machine/colibri-imx7-nand.conf
@@ -22,6 +22,7 @@ PREFERRED_PROVIDER_u-boot ?= "u-boot-toradex"
 PREFERRED_PROVIDER_virtual/bootloader ?= "u-boot-toradex"
 PREFERRED_PROVIDER_u-boot-fw-utils ?= "u-boot-toradex-fw-utils"
 PREFERRED_RPROVIDER_u-boot-fw-utils ?= "u-boot-toradex-fw-utils"
+PREFERRED_PROVIDER_virtual/bootscript ?= "u-boot-script-toradex"
 
 # U-Boot NAND binary includes 0x400 padding required for NAND boot
 UBOOT_BINARY = "u-boot-nand.imx"
@@ -31,7 +32,7 @@ UBOOT_MACHINE ?= "colibri_imx7_defconfig"
 IMAGE_FSTYPES += "tar.xz"
 # wic support
 IMAGE_BOOT_FILES_append = " boot.scr-${MACHINE};boot.scr"
-WKS_FILE_DEPENDS_append = " u-boot-script-toradex"
+WKS_FILE_DEPENDS_append = " virtual/bootscript"
 WKS_FILE = "sdimage-bootpart.wks"
 
 # Enable free --space-fixup (-F) by default, this allows DFU updates

conf/machine/colibri-vf.conf
@@ -22,6 +22,7 @@ PREFERRED_PROVIDER_u-boot ?= "u-boot-toradex"
 PREFERRED_PROVIDER_virtual/bootloader ?= "u-boot-toradex"
 PREFERRED_PROVIDER_u-boot-fw-utils ?= "u-boot-toradex-fw-utils"
 PREFERRED_RPROVIDER_u-boot-fw-utils ?= "u-boot-toradex-fw-utils"
+PREFERRED_PROVIDER_virtual/bootscript ?= "u-boot-script-toradex"
 PREFERRED_PROVIDER_virtual/kernel-module-mcc ?= "kernel-module-mcc-toradex"
 PREFERRED_PROVIDER_virtual/kernel-module-mcc-dev ?= "kernel-module-mcc-toradex"
 PREFERRED_VERSION_mqxboot ?= "1.%"
@@ -34,7 +35,7 @@ UBOOT_MACHINE ?= "colibri_vf_defconfig"
 IMAGE_FSTYPES += "tar.xz ubifs"
 # wic support
 IMAGE_BOOT_FILES_append = " boot.scr-${MACHINE};boot.scr"
-WKS_FILE_DEPENDS_append = " u-boot-script-toradex"
+WKS_FILE_DEPENDS_append = " virtual/bootscript"
 WKS_FILE = "sdimage-bootpart.wks"
 
 # Enable free --space-fixup (-F) by default, this allows DFU updates

recipes-bsp/u-boot/u-boot-script-toradex_2019.07.bb
@@ -38,4 +38,6 @@ do_deploy() {
 
 addtask deploy after do_install before do_build
 
+PROVIDES += "virtual/bootscript"
+
 PACKAGE_ARCH = "${MACHINE_ARCH}"

commit 8d9defa57fb16753e0e29c681bb234f21e17083b

Rename virtual/bootscript to u-boot-default-script
After discussing this in OE-Core we are renaming it so it follows same
naming pattern as u-boot-default-env.

Fixes: 80be160 (u-boot-script-toradex: Set virtual/bootscript as provider)
Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

conf/machine/apalis-imx6.conf
@@ -21,7 +21,7 @@ PREFERRED_PROVIDER_u-boot ?= "u-boot-toradex"
 PREFERRED_PROVIDER_virtual/bootloader ?= "u-boot-toradex"
 PREFERRED_PROVIDER_u-boot-fw-utils ?= "u-boot-toradex-fw-utils"
 PREFERRED_RPROVIDER_u-boot-fw-utils ?= "u-boot-toradex-fw-utils"
-PREFERRED_PROVIDER_virtual/bootscript ?= "u-boot-script-toradex"
+PREFERRED_PROVIDER_u-boot-default-script ?= "u-boot-script-toradex"
 
 SPL_BINARY = "SPL"
 # The SPL configures the DDR RAM depending on the module it runs on. Thus there
@@ -34,7 +34,7 @@ UBOOT_ENTRYPOINT_use-mainline-bsp = "0x10008000"
 IMAGE_FSTYPES += "tar.xz"
 # wic support
 IMAGE_BOOT_FILES_append = " boot.scr-${MACHINE};boot.scr"
-WKS_FILE_DEPENDS_append = " virtual/bootscript"
+WKS_FILE_DEPENDS_append = " u-boot-default-script"
 WKS_FILE = "sdimage-bootpart.wks"
 
 MACHINE_FEATURES += "screen usbgadget usbhost vfat ext2 alsa touchscreen wifi bluetooth 3g pci"

conf/machine/colibri-imx6.conf
@@ -22,7 +22,7 @@ PREFERRED_PROVIDER_u-boot ?= "u-boot-toradex"
 PREFERRED_PROVIDER_virtual/bootloader ?= "u-boot-toradex"
 PREFERRED_PROVIDER_u-boot-fw-utils ?= "u-boot-toradex-fw-utils"
 PREFERRED_RPROVIDER_u-boot-fw-utils ?= "u-boot-toradex-fw-utils"
-PREFERRED_PROVIDER_virtual/bootscript ?= "u-boot-script-toradex"
+PREFERRED_PROVIDER_u-boot-default-script ?= "u-boot-script-toradex"
 
 SPL_BINARY = "SPL"
 UBOOT_CONFIG ??= "spl"
@@ -33,7 +33,7 @@ UBOOT_ENTRYPOINT_use-mainline-bsp = "0x10008000"
 IMAGE_FSTYPES += "tar.xz"
 # wic support
 IMAGE_BOOT_FILES_append = " boot.scr-${MACHINE};boot.scr"
-WKS_FILE_DEPENDS_append = " virtual/bootscript"
+WKS_FILE_DEPENDS_append = " u-boot-default-script"
 WKS_FILE = "sdimage-bootpart.wks"
 
 MACHINE_FEATURES += "screen usbgadget usbhost vfat ext2 alsa touchscreen wifi bluetooth 3g"

conf/machine/colibri-imx6ull.conf
@@ -19,7 +19,7 @@ PREFERRED_PROVIDER_u-boot ?= "u-boot-toradex"
 PREFERRED_PROVIDER_virtual/bootloader ?= "u-boot-toradex"
 PREFERRED_PROVIDER_u-boot-fw-utils ?= "u-boot-toradex-fw-utils"
 PREFERRED_RPROVIDER_u-boot-fw-utils ?= "u-boot-toradex-fw-utils"
-PREFERRED_PROVIDER_virtual/bootscript ?= "u-boot-script-toradex"
+PREFERRED_PROVIDER_u-boot-default-script ?= "u-boot-script-toradex"
 
 UBOOT_BINARY = "u-boot-nand.imx"
 UBOOT_MAKE_TARGET = "u-boot.imx"
@@ -28,7 +28,7 @@ UBOOT_MACHINE ?= "colibri-imx6ull_defconfig"
 IMAGE_FSTYPES += "tar.xz"
 # wic support
 IMAGE_BOOT_FILES_append = " boot.scr-${MACHINE};boot.scr"
-WKS_FILE_DEPENDS_append = " virtual/bootscript"
+WKS_FILE_DEPENDS_append = " u-boot-default-script"
 WKS_FILE = "sdimage-bootpart.wks"
 
 MACHINE_FEATURES += "screen usbgadget usbhost vfat ext2 alsa touchscreen wifi bluetooth 3g"

conf/machine/colibri-imx7-emmc.conf
@@ -18,11 +18,11 @@ RDEPENDS_${KERNEL_PACKAGE_NAME}-base = ""
 PREFERRED_PROVIDER_u-boot ?= "u-boot-toradex"
 PREFERRED_PROVIDER_virtual/bootloader ?= "u-boot-toradex"
 PREFERRED_PROVIDER_u-boot-fw-utils ?= "u-boot-toradex-fw-utils"
-PREFERRED_PROVIDER_virtual/bootscript ?= "u-boot-script-toradex"
+PREFERRED_PROVIDER_u-boot-default-script ?= "u-boot-script-toradex"
 UBOOT_MACHINE ?= "colibri_imx7_emmc_defconfig"
 
 IMAGE_FSTYPES += "tar.xz"
 IMAGE_BOOT_FILES_append = " boot.scr-${MACHINE};boot.scr"
-WKS_FILE_DEPENDS_append = " virtual/bootscript"
+WKS_FILE_DEPENDS_append = " u-boot-default-script"
 
 MACHINE_FEATURES += "screen usbgadget usbhost vfat ext2 alsa touchscreen wifi bluetooth 3g"

conf/machine/colibri-imx7-nand.conf
@@ -22,7 +22,7 @@ PREFERRED_PROVIDER_u-boot ?= "u-boot-toradex"
 PREFERRED_PROVIDER_virtual/bootloader ?= "u-boot-toradex"
 PREFERRED_PROVIDER_u-boot-fw-utils ?= "u-boot-toradex-fw-utils"
 PREFERRED_RPROVIDER_u-boot-fw-utils ?= "u-boot-toradex-fw-utils"
-PREFERRED_PROVIDER_virtual/bootscript ?= "u-boot-script-toradex"
+PREFERRED_PROVIDER_u-boot-default-script ?= "u-boot-script-toradex"
 
 # U-Boot NAND binary includes 0x400 padding required for NAND boot
 UBOOT_BINARY = "u-boot-nand.imx"
@@ -32,7 +32,7 @@ UBOOT_MACHINE ?= "colibri_imx7_defconfig"
 IMAGE_FSTYPES += "tar.xz"
 # wic support
 IMAGE_BOOT_FILES_append = " boot.scr-${MACHINE};boot.scr"
-WKS_FILE_DEPENDS_append = " virtual/bootscript"
+WKS_FILE_DEPENDS_append = " u-boot-default-script"
 WKS_FILE = "sdimage-bootpart.wks"
 
 # Enable free --space-fixup (-F) by default, this allows DFU updates

conf/machine/colibri-vf.conf
@@ -22,7 +22,7 @@ PREFERRED_PROVIDER_u-boot ?= "u-boot-toradex"
 PREFERRED_PROVIDER_virtual/bootloader ?= "u-boot-toradex"
 PREFERRED_PROVIDER_u-boot-fw-utils ?= "u-boot-toradex-fw-utils"
 PREFERRED_RPROVIDER_u-boot-fw-utils ?= "u-boot-toradex-fw-utils"
-PREFERRED_PROVIDER_virtual/bootscript ?= "u-boot-script-toradex"
+PREFERRED_PROVIDER_u-boot-default-script ?= "u-boot-script-toradex"
 PREFERRED_PROVIDER_virtual/kernel-module-mcc ?= "kernel-module-mcc-toradex"
 PREFERRED_PROVIDER_virtual/kernel-module-mcc-dev ?= "kernel-module-mcc-toradex"
 PREFERRED_VERSION_mqxboot ?= "1.%"
@@ -35,7 +35,7 @@ UBOOT_MACHINE ?= "colibri_vf_defconfig"
 IMAGE_FSTYPES += "tar.xz ubifs"
 # wic support
 IMAGE_BOOT_FILES_append = " boot.scr-${MACHINE};boot.scr"
-WKS_FILE_DEPENDS_append = " virtual/bootscript"
+WKS_FILE_DEPENDS_append = " u-boot-default-script"
 WKS_FILE = "sdimage-bootpart.wks"
 
 # Enable free --space-fixup (-F) by default, this allows DFU updates

recipes-bsp/u-boot/u-boot-script-toradex_2019.07.bb
@@ -38,7 +38,7 @@ do_deploy() {
 
 addtask deploy after do_install before do_build
 
-PROVIDES += "virtual/bootscript"
+PROVIDES += "u-boot-default-script"
 
 PACKAGE_ARCH = "${MACHINE_ARCH}"
 