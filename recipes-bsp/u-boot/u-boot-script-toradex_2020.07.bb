Freescale/meta-freescale-3rdparty

commit 671aee4b2baec04e7c81b173c5b4fcf66ece43c0

Generalize overrides subsystem for NXP and Mainline support
This apply the changes from the script in the referred PR.

Refs: https://github.com/Freescale/meta-freescale/pull/980
Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

recipes-bsp/u-boot/u-boot-script-toradex_2020.07.bb
@@ -43,4 +43,4 @@ PROVIDES += "u-boot-default-script"
 
 PACKAGE_ARCH = "${MACHINE_ARCH}"
 
-COMPATIBLE_MACHINE = "(apalis-imx6|colibri-imx6|colibri-imx6ull|colibri-imx7-emmc|colibri-imx7-nand|colibri-vf)"
+COMPATIBLE_MACHINE = "(apalis-imx6-nxp-bsp|colibri-imx6-nxp-bsp|colibri-imx6ull-nxp-bsp|colibri-imx7-emmc|colibri-imx7-nand|colibri-vf)"

recipes-bsp/imx-atf/imx-atf-boundary_2.3.bb
@@ -21,10 +21,10 @@ inherit deploy
 BOOT_TOOLS = "imx-boot-tools"
 
 ATF_PLATFORM        ?= "INVALID"
-ATF_PLATFORM:mx8mq   = "imx8mq"
-ATF_PLATFORM:mx8mm   = "imx8mm"
-ATF_PLATFORM:mx8mn   = "imx8mn"
-ATF_PLATFORM:mx8mp   = "imx8mp"
+ATF_PLATFORM:mx8mq-nxp-bsp   = "imx8mq"
+ATF_PLATFORM:mx8mm-nxp-bsp   = "imx8mm"
+ATF_PLATFORM:mx8mn-nxp-bsp   = "imx8mn"
+ATF_PLATFORM:mx8mp-nxp-bsp   = "imx8mp"
 
 EXTRA_OEMAKE += " \
     CROSS_COMPILE="${TARGET_PREFIX}" \

recipes-bsp/u-boot/u-boot-boundary_2020.10.bb
@@ -1,9 +1,9 @@
 require recipes-bsp/u-boot/u-boot.inc
 require recipes-bsp/u-boot/u-boot-boundary-common_${PV}.inc
 
-FILESEXTRAPATHS:prepend:mx6 := "${THISDIR}/${PN}/arm:"
-FILESEXTRAPATHS:prepend:mx7 := "${THISDIR}/${PN}/arm:"
-FILESEXTRAPATHS:prepend:mx8 := "${THISDIR}/${PN}/aarch64:"
+FILESEXTRAPATHS:prepend:mx6-nxp-bsp := "${THISDIR}/${PN}/arm:"
+FILESEXTRAPATHS:prepend:mx7-nxp-bsp := "${THISDIR}/${PN}/arm:"
+FILESEXTRAPATHS:prepend:mx8-nxp-bsp := "${THISDIR}/${PN}/aarch64:"
 
 DEPENDS += "bison-native"
 
@@ -13,7 +13,7 @@ PROVIDES += "u-boot"
 
 BOOT_TOOLS = "imx-boot-tools"
 
-do_deploy:append:mx8 () {
+do_deploy:append:mx8-nxp-bsp () {
 	install -d ${DEPLOYDIR}/${BOOT_TOOLS}
 	install -m 0777 ${B}/${config}/arch/arm/dts/${UBOOT_DTB_NAME}  ${DEPLOYDIR}/${BOOT_TOOLS}
 	install -m 0777 ${B}/${config}/u-boot-nodtb.bin  ${DEPLOYDIR}/${BOOT_TOOLS}/u-boot-nodtb.bin-${MACHINE}-${UBOOT_CONFIG}

recipes-bsp/u-boot/u-boot-toradex_2020.07.bb
@@ -54,4 +54,4 @@ do_compile:append:colibri-vf () {
 
 PACKAGE_ARCH = "${MACHINE_ARCH}"
 
-COMPATIBLE_MACHINE = "(mx6|mx7|vf|use-mainline-bsp)"
+COMPATIBLE_MACHINE = "(mx6-nxp-bsp|mx7-nxp-bsp|vf-generic-bsp|use-mainline-bsp)"

recipes-kernel/linux/linux-toradex_5.4-2.3.x.bb
@@ -21,4 +21,4 @@ SRCBRANCH = "toradex_5.4-2.3.x-imx"
 # Deploy symbols to allow for device tree overlays
 EXTRA_OEMAKE += "DTC_FLAGS=-@ "
 
-COMPATIBLE_MACHINE = "(mx6|mx7|mx8)"
+COMPATIBLE_MACHINE = "(mx6-nxp-bsp|mx7-nxp-bsp|mx8-nxp-bsp)"

recipes-kernel/linux/linux-variscite_4.14.bb
@@ -16,4 +16,4 @@ SRC_URI = "\
 
 KERNEL_EXTRA_ARGS += "LOADADDR=${UBOOT_ENTRYPOINT}"
 
-COMPATIBLE_MACHINE = "(mx6)"
+COMPATIBLE_MACHINE = "(mx6-nxp-bsp)"

commit bf7ae437e6dfdfc2f5bbc22cdd93bee2b69f44b7

u-boot-script-toradex: Revert wrong override change
Fixes: 671aee4b2b ("Generalize overrides subsystem for NXP and Mainline support")
Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>
Reported-by: Max Krummenacher <max.krummenacher@toradex.com>

recipes-bsp/u-boot/u-boot-script-toradex_2020.07.bb
@@ -43,4 +43,4 @@ PROVIDES += "u-boot-default-script"
 
 PACKAGE_ARCH = "${MACHINE_ARCH}"
 
-COMPATIBLE_MACHINE = "(apalis-imx6-nxp-bsp|colibri-imx6-nxp-bsp|colibri-imx6ull-nxp-bsp|colibri-imx7-emmc|colibri-imx7-nand|colibri-vf)"
+COMPATIBLE_MACHINE = "(apalis-imx6|colibri-imx6|colibri-imx6ull|colibri-imx7-emmc|colibri-imx7-nand|colibri-vf)"